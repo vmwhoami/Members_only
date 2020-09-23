668db4277940c14470e21f043f325867b6fdf9e1
vmwhoami <vmwhoami@gmail.com>
2020-09-23 15:15:56 +0300

    Add custom devise routses with path: path_names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  resources :posts, only: [:index, :new, :create]
-  devise_for :users
+  devise_for :users, path: '', path_names:{sign_in: 'login', sign_up:'register'}
   root 'posts#index'
 end
