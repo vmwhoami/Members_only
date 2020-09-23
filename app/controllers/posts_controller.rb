58221a47438c347b63b3d886434e79be1202cb48
vmwhoami <vmwhoami@gmail.com>
2020-09-23 13:03:19 +0300

    Create all the functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,30 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
 
   def create
+    @post = Post.new(permited_posts)
+    @post.user = current_user
+    if @post.save
+      flash[:succes] = "Your post has been created"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Your post has not been created"
+      render :new
+    end
   end
 
-  def index
-  end
+
+
+  private
+def permited_posts
+  params.require(:post).permit(:body)
+end
 end
