e69974e0967099c515338448820691c12aaa9de3
vmwhoami <vmwhoami@gmail.com>
2020-09-25 11:22:51 +0300

    Remove logic from the views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+
+  def postul(post)
+    post.body 
+  end
+  def post_authot(post)
+  "by " + post.user.name if user_signed_in?
+  end
+ 
+  def create_new_post
+    link_to  "Create new post", new_post_path if user_signed_in?
+  end
+
 end
