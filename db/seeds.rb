116129094ad2f0d3c490118a499981508ee0a885
Vitalie Melnic <vmwhoami@gmail.com>
2020-09-25 18:18:11 +0300

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.create(name:"Vitalie", email:"vmwhoami@gmail.com", password:"password")
+puts "User ceated"
+5.times do |post|
+  Post.create(body: "
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Eius cum, sunt magni debitis ipsa, quo corporis commodi quod ad
+   magnam aperiam minus enim voluptas. Nihil tenetur perspiciatis minima 
+   eveniet recusandae.",
+   user_id: u.id
+   )
+
+end
+
+puts "Posts created"
